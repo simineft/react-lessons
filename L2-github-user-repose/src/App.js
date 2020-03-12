9b8bbeee0aeef717dc7e0a43a86443ee71e4f816
Simin Eftekhari <simineftekhari95@gmail.com>
2020-03-13 02:10:28 +0330

    fix: project title

---
+++ L2-github-user-repose/src/App.js
@@ -0,0 +1,30 @@
+import React,{ Component } from 'react';
+import User from "./components/User";
+import './App.css';
+
+
+
+
+class App extends Component {
+
+
+  render() {
+    
+    return (
+
+      <div >
+        <header className="app-git">
+          <h1>User GitHub Project </h1>
+        </header>
+
+        <User />
+
+      </div>
+
+
+
+    );
+  }
+}
+
+export default App;
